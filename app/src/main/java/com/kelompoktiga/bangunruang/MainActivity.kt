54bfaca766dde9a1857e3dfb604c58f1b72442e2
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-09-03 12:34:11 +0700

    feat: make calculate button functional

--- app/src/main/java/com/kelompoktiga/bangunruang/MainActivity.kt
+++ app/src/main/java/com/kelompoktiga/bangunruang/MainActivity.kt
@@ -2,6 +2,11 @@ package com.kelompoktiga.bangunruang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import kotlin.math.pow
 import kotlin.math.roundToInt
 
@@ -10,8 +15,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fun calculate(surface: Int): Int {
+        fun cubeVolume(surface: Int): Int {
             return surface.toDouble().pow(3.0).roundToInt()
+        }
+
+        val surfaceInput = findViewById<EditText>(R.id.surfaceEdit)
+        val calcButton = findViewById<Button>(R.id.btnCalculate)
+        val resView = findViewById<TextView>(R.id.resultView)
+
+        calcButton.setOnClickListener {
+            if (TextUtils.isEmpty(surfaceInput.text.toString())) {
+                Toast.makeText(this, "Empty field is not allowed!", Toast.LENGTH_SHORT).show()
+            } else {
+                val result = cubeVolume(surfaceInput.text.toString().toInt())
+                Toast.makeText(this@MainActivity, "Calculating...", Toast.LENGTH_SHORT).show()
+                resView.text = result.toString()
+            }
         }
     }
 }
