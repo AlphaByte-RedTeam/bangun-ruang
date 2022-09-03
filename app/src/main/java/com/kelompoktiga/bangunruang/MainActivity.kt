f3d00241cabf1578343c0ed94cb3ad6de2a3a098
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-09-03 11:40:09 +0700

    feat: add calculate function

--- app/src/main/java/com/kelompoktiga/bangunruang/MainActivity.kt
+++ app/src/main/java/com/kelompoktiga/bangunruang/MainActivity.kt
@@ -2,10 +2,16 @@ package com.kelompoktiga.bangunruang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.math.pow
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fun calculate(surface: Int): Int {
+            return surface.toDouble().pow(3.0).roundToInt()
+        }
     }
 }
